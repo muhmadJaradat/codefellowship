bd12a97bfb668df81a23f0cf5a8ed010723c3b8e
muhmadjaradat <muhmadjradat@gmail.com>
2021-07-29 00:55:53 +0300

    adding lab18

--- src/main/java/com/codefellowship/Controllers/HomePage.java
+++ src/main/java/com/codefellowship/Controllers/HomePage.java
@@ -1,5 +1,8 @@
 package com.codefellowship.Controllers;
 
+import com.codefellowship.Models.ApplicationUser;
+import com.codefellowship.Repositories.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -8,12 +11,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 
+
+
 @Controller
 public class HomePage {
 
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
     @GetMapping
     public String getHomePage(Principal principal, Model model){
-        if (principal!=null)model.addAttribute("userName",principal.getName());
+
+        if (principal!=null){
+            ApplicationUser loggedUser=applicationUserRepository.findApplicationUserByUsername(principal.getName());
+            model.addAttribute("userName",principal.getName());
+            model.addAttribute("loggedUser",loggedUser);
+        }
+        model.addAttribute("users",applicationUserRepository.findAll());
         return "Home";
+    }
+
+    @GetMapping("/feed")
+    public String feedPage(Principal principal,Model model){
+        if (principal!=null){
+            ApplicationUser user=applicationUserRepository.findApplicationUserByUsername(principal.getName());
+            model.addAttribute("user",user);
+        }
+        return "feed";
     }
 }
